f9e33143da94bb573152018fdbe1576f943dc031
Solutech11 <soluwizy@gmail.com>
2023-01-12 21:35:45 +0100

    h

--- App.js
+++ App.js
@@ -8,4 +8,4 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => res.render('home'))
-app.listen(port, () => console.log('server running'))
+app.listen(port, () => console.log('server running ohh'))
