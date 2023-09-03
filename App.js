e178fb90b93f1f6ee3c71f4aa1e825ed527eff1b
Solutech11 <soluwizy@gmail.com>
2023-09-03 22:27:32 +0100

    update

--- App.js
+++ App.js
@@ -7,5 +7,19 @@ app.use(require('cors')())
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
-app.get('/', (req, res) => res.render('home'))
+app.get('/', (req, res) => {
+    try {
+        res.render("index")
+    } catch (error) {
+        res.status(404).render('404')
+    }
+})
+
+app.get('/:rt', (req, res) => {
+    try {
+        res.render(req.params.rt?req.params.rt:"index")
+    } catch (error) {
+        res.status(404).render('404')
+    }
+})
 app.listen(port, () => console.log('server running'))
